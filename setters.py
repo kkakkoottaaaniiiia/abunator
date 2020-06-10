e02bab8a0c091277291428e27fa76a9925612c3d
OhkiShinomiya <e6akita@yahoo.co.jp>
2020-06-10 09:58:23 +0900

    0957 by Dohi

---
+++ setters.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 10 09:50:58 2020
+
+@author: School
+"""
+import sqlMethods
+import counter
+import Branch
+
+
+def setPathList():
+    Branch.PathList.clear()
+    Branch.PathList = [0]
+
+def setColumnList():
+    counter.ColumnList.clear()
+
+def setQuestionList():
+    counter.QuestionList.clear()
+
+def setSQLList():
+    counter.SQLList.clear()
+
